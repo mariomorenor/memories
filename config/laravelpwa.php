261a833cb3af654182cebce6de4ed62ee942f7fa
Jonathan Moreno <jmoreno@gruposcanner.com>
2020-12-25 21:26:30 -0500

    PWA Correcto

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -5,7 +5,7 @@ return [
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
         'short_name' => 'Memories',
-        'start_url' => '/login',
+        'start_url' => "/",
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
         'display' => 'standalone',
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/memories-icons/hdpi.png',
+                'path' => 'images/icons/hdpi.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/memories-icons/xhdpi.png',
+                'path' => 'images/icons/xhdpi.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => 'images/icons/xxhdpi.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/memories-icons/xxhdpi.png',
+                'path' => 'images/icons/xxxhdpi.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/memories-icons/.png',
+                'path' => 'images/icons/xxhdpi.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/memories-icons/xxxhdpi.png',
+                'path' => 'images/icons/xxxhdpi.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => 'images/icons/xxxhdpi.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/memories-icons/GooglePlayStore.png',
+                'path' => 'images/icons/GooglePlayStore.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => 'images/icons/splash-640x1136.png',
+            '750x1334' => 'images/icons/splash-750x1334.png',
+            '828x1792' => 'images/icons/splash-828x1792.png',
+            '1125x2436' => 'images/icons/splash-1125x2436.png',
+            '1242x2208' => 'images/icons/splash-1242x2208.png',
+            '1242x2688' => 'images/icons/splash-1242x2688.png',
+            '1536x2048' => 'images/icons/splash-1536x2048.png',
+            '1668x2224' => 'images/icons/splash-1668x2224.png',
+            '1668x2388' => 'images/icons/splash-1668x2388.png',
+            '2048x2732' => 'images/icons/splash-2048x2732.png',
         ],
         'shortcuts' => [
             [
@@ -63,7 +63,7 @@ return [
                 'description' => 'Shortcut Link 1 Description',
                 'url' => '/shortcutlink1',
                 'icons' => [
-                    "src" => "/images/icons/icon-72x72.png",
+                    "src" => "images/icons/GooglePlayStore.png",
                     "purpose" => "any"
                 ]
             ],
